4900194e99b987be2653e94ead4aaca6dfe96ff4
Dre Cura <71569154+kako57@users.noreply.github.com>
2023-07-05 13:01:16 -0400

    big tabs

--- lua/kako57/set.lua
+++ lua/kako57/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
